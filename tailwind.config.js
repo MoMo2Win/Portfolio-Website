ccff5907bf38240d6ca5ffb0f59fa29f1e4ab259
20128094 <20128094@tafe.wa.edu.au>
2024-11-20 13:08:45 +0800

    feat(home-page): implemented tailwindcss to the home-page for the mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
       './*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        LGB: "#E0E7FF",
+        SG: "#10B981",
+      }
+    },
   },
   plugins: [],
 }
